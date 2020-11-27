dab69818c3357f4dba3926b0f1dcf9c7ee8b8e3b
Eugene-36 <67147219+Eugene-36@users.noreply.github.com>
2020-11-27 10:49:58 +0200

    Новые настройки для вебпака

--- postcss.config.js
+++ postcss.config.js
@@ -1,4 +1,3 @@
-import css from "./css/styles.css";
 module.exports = {
   plugins: [require("autoprefixer")],
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,54 +1,57 @@
-const path = require("path")
-const {merge} = require("webpack-merge")
-const{CleanWebpackPlugin} = require("clean-webpack-plagin")
-const FriendlyErrorsWebpackPlugin = require("friendly-errors-webpack-plugin")
-const WebpackBar =require("webpack")
+const path = require("path");
+const { merge } = require("webpack-merge");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const FriendlyErrorsWebpackPlugin = require("friendly-errors-webpack-plugin");
+const WebpackBar = require("webpackbar");
 
 const loadModeConfig = (env) =>
   require(`./build-utils/${env.mode}.config.js`)(env);
 
-module.exports = (env) => merge({
-    mode: env.mode,
-    context:path.resolve(__dirname, "src"),
+module.exports = (env) =>
+  merge(
+    {
+      mode: env.mode,
+      context: path.resolve(__dirname, "src"),
 
-    entry: './index.js',
-    output: {
+      entry: "./index.js",
+      output: {
         path: path.resolve(__dirname, "dist"),
         filename: "[name].bundle.js",
-    },
-    module:{
-        rules:[
-            {
-                test:/\.js$/,
-                exclude: /node_modules/,
-                use: ["babel-loader"]
-            },
-            {
-                test: /\.html$/,
-                use: ["html-loader"]
-            },
-            {
-                test:/\.(gif|png|jpe?g|svg)$/,
-                use: [{
-                    loader: "url-loader",
-                    options: {
-                        name: "[path]/[name].[ext]",
-                        limit: false,
-                    }
-                }]
-            },
-            {
-                test:/\.hbs$/,
-                use: ["handlebars-loader"],
-            }
+      },
+      module: {
+        rules: [
+          {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: ["babel-loader"],
+          },
+          {
+            test: /\.html$/,
+            use: ["html-loader"],
+          },
+          {
+            test: /\.(gif|png|jpe?g|svg)$/,
+            use: [
+              {
+                loader: "url-loader",
+                options: {
+                  name: "[path]/[name].[ext]",
+                  limit: false,
+                },
+              },
+            ],
+          },
+          {
+            test: /\.hbs$/,
+            use: ["handlebars-loader"],
+          },
         ],
-
-    },
-    plugins:[
+      },
+      plugins: [
         new CleanWebpackPlugin(),
         new FriendlyErrorsWebpackPlugin(),
-        new WebpackBar() 
-    ]
-},
-loadModeConfig(env)
-)
+        new WebpackBar(),
+      ],
+    },
+    loadModeConfig(env),
+  );
